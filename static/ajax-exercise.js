b0eb00f0b4360181694b989d54ac56080bb0edd7
Rayna Mecham <rayna.mecham@gmail.com>
2021-03-29 18:01:39 -0600

    changed to arrow functions

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -3,18 +3,31 @@
 
 // PART 1: SHOW A FORTUNE
 
-function showFortune(evt) {
+// function replaceFortune(results) {
+//     $("#fortune-text").html(results);
+// }
 
-    // TODO: get the fortune and show it in the #fortune-text div
+let replaceFortune = (results) => {
+    $("#fortune-text").html(results);
+};
+
+function showFortune(evt) { 
+    $.get('/fortune', replaceFortune);
 }
 
 $('#get-fortune-button').on('click', showFortune);
 
 
 
-
-
 // PART 2: SHOW WEATHER
+
+// function replaceWeather(results) {
+//     $("#weather-info").html(results.forecast);
+// }
+
+let replaceWeather = (results) => {
+    $("#weather-info").html(results.forecast);
+};
 
 function showWeather(evt) {
     evt.preventDefault();
@@ -23,7 +36,7 @@ function showWeather(evt) {
     let formData = {"zipcode": $("#zipcode-field").val()};
 
 
-    // TODO: request weather with that URL and show the forecast in #weather-info
+    $.get(url, formData, replaceWeather);
 }
 
 $("#weather-form").on('submit', showWeather);
@@ -33,13 +46,36 @@ $("#weather-form").on('submit', showWeather);
 
 // PART 3: ORDER MELONS
 
+// function updateMelons(results) {
+//     if (results.code === "OK") {
+//         $('#order-status').html("<p>" + results.msg + "</p>");
+//     }
+//     else {
+//         $('#order-status').addClass("order-error");
+//         $('#order-status').html("<p><b>" + results.msg + "</b></p>");
+//     }
+// }
+
+let updateMelons = (results) => {
+    if (results.code === "OK") {
+        $('#order-status').html("<p>" + results.msg + "</p>");
+    }
+    else {
+        $('#order-status').addClass("order-error");
+        $('#order-status').html("<p><b>" + results.msg + "</b></p>");
+    }
+};
+
 function orderMelons(evt) {
     evt.preventDefault();
 
-    // TODO: show the result message after your form
-    // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
+    let formInputs = {
+        "melon_type": $("#melon-type-field").val(),
+        "qty": $("#qty-field").val()
+    };
+
+    $.post("/order-melons.json", formInputs, updateMelons);
 }
 
 $("#order-form").on('submit', orderMelons);
-
 
